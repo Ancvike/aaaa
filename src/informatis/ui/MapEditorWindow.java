00c6f693393ca1faf15cc82b49bc5b40c150eee4
Ancvike <2047870696@qq.com>
2025-01-21 17:51:41 +0800

    a

--- src/informatis/ui/MapEditorWindow.java
+++ src/informatis/ui/MapEditorWindow.java
@@ -22,20 +22,18 @@ import mindustry.gen.*;
 import mindustry.ui.*;
 import mindustry.world.*;
 
-import static informatis.ui.WindowManager.mapEditorWindow;
 import static mindustry.Vars.*;
 
 public class MapEditorWindow extends Window {
     EditorTool tool;
     final Vec2[][] brushPolygons = new Vec2[MapEditor.brushSizes.length][0];
     float heat;
-    float brushSize = -1;
+    float brushSize = 2;
 
     boolean drawing;
     int lastx, lasty;
     float lastw, lasth;
 
-    public static Team drawTeam = Team.sharded;
     public static Block drawBlock = Blocks.router;
 
     public MapEditorWindow() {
@@ -79,26 +77,9 @@ public class MapEditorWindow extends Window {
             }
             Lines.stroke(Scl.scl(2f), Pal.accent);
 
-            if ((!drawBlock.isMultiblock() || tool == EditorTool.eraser) && tool != EditorTool.fill) {
-                if (tool == EditorTool.line && drawing) {
-                    Lines.poly(brushPolygons[index], lastx, lasty, scaling);
-                    Lines.poly(brushPolygons[index], tile.x * 8, tile.y * 8, scaling);
-                }
-
-                if ((tool.edit || (tool == EditorTool.line && !drawing)) && (!mobile || drawing)) {
-                    if (tool == EditorTool.pencil && tool.mode == 1) {
-                        Lines.square(tile.x * 8, tile.y * 8, scaling * (brushSize + 0.5f));
-                    } else {
-                        Lines.poly(brushPolygons[index], tile.x * 8 - 4, tile.y * 8 - 4, scaling);
-                    }
-                }
-            } else {
-                if ((tool.edit || tool == EditorTool.line) && (!mobile || drawing)) {
-                    float offset = (drawBlock.size % 2 == 0 ? scaling / 2f : 0f);
-                    Lines.square(
-                            tile.x * 8 + scaling / 2f + offset,
-                            tile.y * 8 + scaling / 2f + offset,
-                            scaling * drawBlock.size / 2f);
+            if (!drawBlock.isMultiblock() || tool == EditorTool.eraser) {
+                if (tool.edit && (!mobile || drawing)) {
+                    Lines.poly(brushPolygons[index], tile.x * 8 - 4, tile.y * 8 - 4, scaling);
                 }
             }
         });
@@ -118,13 +99,11 @@ public class MapEditorWindow extends Window {
             Tile tile = world.tileWorld(Core.input.mouseWorldX(), Core.input.mouseWorldY());
             if (tile == null || tool == null || brushSize < 1 || drawBlock == null || hasMouse()) return;
             if (Core.input.isTouched()) {
-                if ((tool == EditorTool.line && drawing) || (!mobile && !Core.input.keyDown(KeyCode.mouseLeft))) return;
+                if (!mobile && !Core.input.keyDown(KeyCode.mouseLeft)) return;
                 drawing = true;
                 lastx = tile.x;
                 lasty = tile.y;
-                tool.touched(lastx, lasty);
             } else {
-                if (tool == EditorTool.line && drawing) tool.touchedLine(lastx, lasty, tile.x, tile.y);
                 drawing = false;
                 lastx = -1;
                 lasty = -1;
@@ -186,11 +165,7 @@ public class MapEditorWindow extends Window {
                             bt.stack(button, mode);
                         };
 
-                        addTool.get(EditorTool.line);
-                        addTool.get(EditorTool.pencil);
                         addTool.get(EditorTool.eraser);
-                        addTool.get(EditorTool.fill);
-                        addTool.get(EditorTool.spray);
 
                         ImageButton grid = new ImageButton(Icon.grid, Styles.clearTogglei);
                         grid.clicked(() -> {
@@ -241,304 +216,10 @@ public class MapEditorWindow extends Window {
         return getWidth() - (find("buttons") == null ? 1 : find("buttons").getWidth());
     }
 
-    public void drawBlocksReplace(int x, int y) {
-        drawBlocks(x, y, tile -> tile.block() != Blocks.air || drawBlock.isFloor());
-    }
-
-    public void drawBlocks(int x, int y) {
-        drawBlocks(x, y, false, tile -> true);
-    }
-
-    public void drawBlocks(int x, int y, Boolf<Tile> tester) {
-        drawBlocks(x, y, false, tester);
-    }
-
-    final int rotation = 0;
-
-    public void drawBlocks(int x, int y, boolean square, Boolf<Tile> tester) {
-        if (drawBlock.isMultiblock()) {
-            x = Mathf.clamp(x, (drawBlock.size - 1) / 2, world.width() - drawBlock.size / 2 - 1);
-            y = Mathf.clamp(y, (drawBlock.size - 1) / 2, world.height() - drawBlock.size / 2 - 1);
-            Tile tile = world.tile(x, y);
-            if (tile != null && !hasOverlap(tile)) {
-                tile.setBlock(drawBlock, drawTeam, rotation);
-            }
-        } else {
-            boolean isFloor = drawBlock.isFloor() && drawBlock != Blocks.air;
-
-            Cons<Tile> drawer = tile -> {
-                if (!tester.get(tile)) return;
-
-                if (isFloor) {
-                    tile.setFloor(drawBlock.asFloor());
-                } else if (!(tile.block().isMultiblock() && !drawBlock.isMultiblock())) {
-                    tile.setBlock(drawBlock, drawTeam, rotation);
-                }
-            };
-
-            if (square) {
-                drawSquare(x, y, drawer);
-            } else {
-                drawCircle(x, y, drawer);
-            }
-        }
-    }
-
-    public void drawCircle(int x, int y, Cons<Tile> drawer) {
-        int clamped = (int) brushSize;
-        for (int rx = -clamped; rx <= clamped; rx++) {
-            for (int ry = -clamped; ry <= clamped; ry++) {
-                if (Mathf.within(rx, ry, brushSize - 0.5f + 0.0001f)) {
-                    int wx = x + rx, wy = y + ry;
-
-                    if (wx < 0 || wy < 0 || wx >= world.width() || wy >= world.height()) {
-                        continue;
-                    }
-
-                    drawer.get(world.tile(wx, wy));
-                }
-            }
-        }
-    }
-
-    public void drawSquare(int x, int y, Cons<Tile> drawer) {
-        int clamped = (int) brushSize;
-        for (int rx = -clamped; rx <= clamped; rx++) {
-            for (int ry = -clamped; ry <= clamped; ry++) {
-                int wx = x + rx, wy = y + ry;
-
-                if (wx < 0 || wy < 0 || wx >= world.width() || wy >= world.height()) {
-                    continue;
-                }
-
-                drawer.get(world.tile(wx, wy));
-            }
-        }
-    }
-
-    boolean hasOverlap(Tile tile) {
-        //allow direct replacement of blocks of the same size
-        if (tile.isCenter() && tile.block() != drawBlock && tile.block().size == drawBlock.size && tile.x == x && tile.y == y) {
-            return false;
-        }
-
-        //else, check for overlap
-        int offsetX = -(drawBlock.size - 1) / 2;
-        int offsetY = -(drawBlock.size - 1) / 2;
-        for (int dx = 0; dx < drawBlock.size; dx++) {
-            for (int dy = 0; dy < drawBlock.size; dy++) {
-                int worldX = dx + offsetX + tile.x;
-                int worldY = dy + offsetY + tile.y;
-                Tile other = world.tile(worldX, worldY);
-
-                if (other != null && other.block().isMultiblock()) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
     enum EditorTool {
-        line(KeyCode.l, "replace", "orthogonal") {
-            @Override
-            public void touchedLine(int x1, int y1, int x2, int y2) {
-                //straight
-                if (mode == 1) {
-                    if (Math.abs(x2 - x1) > Math.abs(y2 - y1)) {
-                        y2 = y1;
-                    } else {
-                        x2 = x1;
-                    }
-                }
-
-                Bresenham2.line(x1, y1, x2, y2, (x, y) -> {
-                    if (mode == 0) {
-                        //replace
-                        mapEditorWindow.drawBlocksReplace(x, y);
-                    } else {
-                        //normal
-                        mapEditorWindow.drawBlocks(x, y);
-                    }
-                });
-            }
-        },
-        pencil(KeyCode.b, "replace", "square", "drawteams") {
+        eraser("eraseores") {
             {
                 edit = true;
-                draggable = true;
-            }
-
-            @Override
-            public void touched(int x, int y) {
-                if (mode == -1) {
-                    //normal mode
-                    mapEditorWindow.drawBlocks(x, y);
-                } else if (mode == 0) {
-                    //replace mode
-                    mapEditorWindow.drawBlocksReplace(x, y);
-                } else if (mode == 1) {
-                    //square mode
-                    mapEditorWindow.drawBlocks(x, y, true, tile -> true);
-                } else if (mode == 2) {
-                    //draw teams
-                    mapEditorWindow.drawCircle(x, y, tile -> tile.setTeam(drawTeam));
-                }
-
-            }
-        },
-        eraser(KeyCode.e, "eraseores") {
-            {
-                edit = true;
-                draggable = true;
-            }
-
-            @Override
-            public void touched(int x, int y) {
-                mapEditorWindow.drawCircle(x, y, tile -> {
-                    if (mode == -1) {
-                        //erase block
-                        tile.remove();
-                    } else if (mode == 0) {
-                        //erase ore
-                        tile.clearOverlay();
-                    }
-                });
-            }
-        },
-        fill(KeyCode.g, "replaceall", "fillteams") {
-            {
-                edit = true;
-            }
-
-            IntSeq stack = new IntSeq();
-
-            @Override
-            public void touched(int x, int y) {
-                if (!Structs.inBounds(x, y, world.width(), world.height())) return;
-                Tile tile = world.tile(x, y);
-                if (tile == null) return;
-
-                //mode 0 or 1, fill everything with the floor/tile or replace it
-                if (mode == 0 || mode == -1) {
-                    if (tile.block().isMultiblock()) return;
-
-                    Boolf<Tile> tester;
-                    Cons<Tile> setter;
-                    Block drawBlock = MapEditorWindow.drawBlock;
-
-                    if (drawBlock.isOverlay()) {
-                        Block dest = tile.overlay();
-                        if (dest == drawBlock) return;
-                        tester = t -> t.overlay() == dest && (t.floor().hasSurface() || !t.floor().needsSurface);
-                        setter = t -> t.setOverlay(drawBlock);
-                    } else if (drawBlock.isFloor()) {
-                        Block dest = tile.floor();
-                        if (dest == drawBlock) return;
-                        tester = t -> t.floor() == dest;
-                        setter = t -> t.setFloorUnder(drawBlock.asFloor());
-                    } else {
-                        Block dest = tile.block();
-                        if (dest == drawBlock) return;
-                        tester = t -> t.block() == dest;
-                        setter = t -> t.setBlock(drawBlock, drawTeam);
-                    }
-
-                    //replace only when the mode is 0 using the specified functions
-                    fill(x, y, mode == 0, tester, setter);
-                } else if (mode == 1) { //mode 1 is team fill
-                    //only fill synthetic blocks, it's meaningless otherwise
-                    if (tile.synthetic()) {
-                        Team dest = tile.team();
-                        if (dest == drawTeam) return;
-                        fill(x, y, false, t -> t.getTeamID() == dest.id && t.synthetic(), t -> t.setTeam(drawTeam));
-                    }
-                }
-            }
-
-            void fill(int x, int y, boolean replace, Boolf<Tile> tester, Cons<Tile> filler) {
-                int width = world.width(), height = world.height();
-
-                if (replace) {
-                    //just do it on everything
-                    for (int cx = 0; cx < width; cx++) {
-                        for (int cy = 0; cy < height; cy++) {
-                            Tile tile = world.tile(cx, cy);
-                            if (tester.get(tile)) {
-                                filler.get(tile);
-                            }
-                        }
-                    }
-
-                } else {
-                    //perform flood fill
-                    int x1;
-
-                    stack.clear();
-                    stack.add(Point2.pack(x, y));
-
-                    try {
-                        while (stack.size > 0 && stack.size < width * height) {
-                            int popped = stack.pop();
-                            x = Point2.x(popped);
-                            y = Point2.y(popped);
-
-                            x1 = x;
-                            while (x1 >= 0 && tester.get(world.tile(x1, y))) x1--;
-                            x1++;
-                            boolean spanAbove = false, spanBelow = false;
-                            while (x1 < width && tester.get(world.tile(x1, y))) {
-                                filler.get(world.tile(x1, y));
-
-                                if (!spanAbove && y > 0 && tester.get(world.tile(x1, y - 1))) {
-                                    stack.add(Point2.pack(x1, y - 1));
-                                    spanAbove = true;
-                                } else if (spanAbove && !tester.get(world.tile(x1, y - 1))) {
-                                    spanAbove = false;
-                                }
-
-                                if (!spanBelow && y < height - 1 && tester.get(world.tile(x1, y + 1))) {
-                                    stack.add(Point2.pack(x1, y + 1));
-                                    spanBelow = true;
-                                } else if (spanBelow && y < height - 1 && !tester.get(world.tile(x1, y + 1))) {
-                                    spanBelow = false;
-                                }
-                                x1++;
-                            }
-                        }
-                        stack.clear();
-                    } catch (OutOfMemoryError e) {
-                        //hack
-                        stack = null;
-                        System.gc();
-                        stack = new IntSeq();
-                        Log.err(e);
-                    }
-                }
-            }
-        },
-        spray(KeyCode.r, "replace") {
-            final double chance = 0.012;
-
-            {
-                edit = true;
-                draggable = true;
-            }
-
-            @Override
-            public void touched(int x, int y) {
-                //floor spray
-                if (drawBlock.isFloor()) {
-                    mapEditorWindow.drawCircle(x, y, tile -> {
-                        if (Mathf.chance(chance)) {
-                            tile.setFloor(drawBlock.asFloor());
-                        }
-                    });
-                } else if (mode == 0) { //replace-only mode, doesn't affect air
-                    mapEditorWindow.drawBlocks(x, y, tile -> Mathf.chance(chance) && tile.block() != Blocks.air);
-                } else {
-                    mapEditorWindow.drawBlocks(x, y, tile -> Mathf.chance(chance));
-                }
             }
         };
 
@@ -547,10 +228,6 @@ public class MapEditorWindow extends Window {
          */
         public final String[] altModes;
         /**
-         * Key to activate this tool.
-         */
-        public KeyCode key = KeyCode.unset;
-        /**
          * The current alternate placement mode. -1 is the standard mode, no changes.
          */
         public int mode = -1;
@@ -558,28 +235,9 @@ public class MapEditorWindow extends Window {
          * Whether this tool causes canvas changes when touched.
          */
         public boolean edit;
-        /**
-         * Whether this tool should be dragged across the canvas when the mouse moves.
-         */
-        public boolean draggable;
-
-        EditorTool() {
-            this(new String[]{});
-        }
 
         EditorTool(String... altModes) {
             this.altModes = altModes;
-        }
-
-        EditorTool(KeyCode code, String... altModes) {
-            this.altModes = altModes;
-            this.key = code;
-        }
-
-        public void touched(int x, int y) {
-        }
-
-        public void touchedLine(int x1, int y1, int x2, int y2) {
         }
     }
 }
