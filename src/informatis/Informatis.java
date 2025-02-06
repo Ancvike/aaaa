512b7b6bf12b86162e54b0a115393b8618ecc3df
Ancvike <2047870696@qq.com>
2025-02-06 20:09:51 +0800

    a

--- src/informatis/Informatis.java
+++ src/informatis/Informatis.java
@@ -1,16 +1,10 @@
 package informatis;
 
-import arc.Events;
-import informatis.ui.WindowManager;
-import mindustry.game.EventType.ClientLoadEvent;
 import mindustry.mod.Mod;
 
 public class Informatis extends Mod {
     @Override
     public void init(){
-        Events.on(ClientLoadEvent.class, e -> {
-            WindowManager.init();
 
-        });
     }
 }
